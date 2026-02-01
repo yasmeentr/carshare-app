caa58e9d1369d4429922cc266db930874ab3f382
yasmeentr <yasmeen.trad@etudiant.univ-reims.fr>
2026-02-01 12:44:50 -0500

    Update locustfile.py

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -2,7 +2,7 @@
 from locust import HttpUser, task, between
 
 class WebsiteUser(HttpUser):
-    host = "http://localhost:8090"  # <= CORRIGÉ (http://)
+    host = "http://localhost:8090"  
     wait_time = between(1, 3)
 
     @task
@@ -11,7 +11,6 @@ class WebsiteUser(HttpUser):
 
     @task(3)
     def login_fake(self):
-        # Tomcat/JSP attend souvent du form-urlencoded
         self.client.post("/carshare-app/login", data={
             "email": "bob@example.com",
             "password": "12345"
